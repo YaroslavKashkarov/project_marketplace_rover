d950d8edb4468afa56f879c8ba13ca140955ebcc
Yaroslav Kashkarov <126462120+YaroslavKashkarov@users.noreply.github.com>
2023-11-27 21:19:13 +0100

    created blocks categories of products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
-import {HeaderComponent} from './header/header.component';
+import {HeaderComponent} from './components/header/header.component';
+import {CategoriesOfProductsComponent} from './components/categories-of-products/categories-of-products.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, HeaderComponent, CategoriesOfProductsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
