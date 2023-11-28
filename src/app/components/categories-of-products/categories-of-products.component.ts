f5a095765319b55d0848ccad6fafcd679d046cb6
Yaroslav Kashkarov <126462120+YaroslavKashkarov@users.noreply.github.com>
2023-11-28 21:32:10 +0100

    updeted blocks header and categories of products

--- src/app/components/categories-of-products/categories-of-products.component.ts
+++ src/app/components/categories-of-products/categories-of-products.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-categories-of-products',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './categories-of-products.component.html',
-  styleUrl: './categories-of-products.component.scss'
+  styleUrl: './categories-of-products.component.scss',
 })
 export class CategoriesOfProductsComponent {
 
