17025abcc47a4c011bec7c9bf3c8f95963d53767
Yaroslav Kashkarov <126462120+YaroslavKashkarov@users.noreply.github.com>
2023-12-17 12:14:50 +0100

    deleted component categories of products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
-import {CategoriesOfProductsComponent} from './components/categories-of-products/categories-of-products.component';
 import {CreateAnItemComponent} from './components/create-an-item/create-an-item.component';
 import {FavoriteComponent} from './components/favorite/favorite.component';
 import {HomePageComponent} from './components/home-page/home-page.component';
@@ -9,7 +8,7 @@ import {HomePageComponent} from './components/home-page/home-page.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomePageComponent, HomePageComponent, CategoriesOfProductsComponent, CreateAnItemComponent, FavoriteComponent, HomePageComponent],
+  imports: [CommonModule, RouterOutlet, HomePageComponent, HomePageComponent, CreateAnItemComponent, FavoriteComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
 
