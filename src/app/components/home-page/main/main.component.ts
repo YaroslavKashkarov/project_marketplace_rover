d2ec794f436b22b4034be363f8e781476aa37a17
Yaroslav Kashkarov <126462120+YaroslavKashkarov@users.noreply.github.com>
2023-12-26 20:49:23 +0100

    footer block component layout

--- src/app/components/home-page/main/main.component.ts
+++ src/app/components/home-page/main/main.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
 export class MainComponent {
 
