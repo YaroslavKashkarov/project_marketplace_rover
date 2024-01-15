a10c3a439afa634a4cd226635825e3b7672ea259
Yaroslav Kashkarov <126462120+YaroslavKashkarov@users.noreply.github.com>
2024-01-15 21:15:59 +0100

    work on blocks of the main component (carousel i popular items)

--- src/app/components/home-page/main/main.component.ts
+++ src/app/components/home-page/main/main.component.ts
@@ -1,11 +1,12 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NavbarComponent} from './navbar/navbar.component';
+import {CarouselComponent} from './carousel/carousel.component';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [CommonModule, NavbarComponent],
+  imports: [CommonModule, NavbarComponent, CarouselComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss',
 })
