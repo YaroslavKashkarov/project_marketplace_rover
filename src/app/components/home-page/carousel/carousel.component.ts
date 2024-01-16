2e3fece531b35b1b0dab71b705bea76c03ebdb78
Yaroslav Kashkarov <126462120+YaroslavKashkarov@users.noreply.github.com>
2024-01-16 22:20:22 +0100

    addeted blocks popular items and carousel-prototype

---
+++ src/app/components/home-page/carousel/carousel.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.scss'
+})
+export class CarouselComponent {
+
+}
--- src/app/components/home-page/main/main.component.ts
+++ src/app/components/home-page/main/main.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NavbarComponent} from './navbar/navbar.component';
-import {CarouselComponent} from './carousel/carousel.component';
+import {CarouselComponent} from '../carousel/carousel.component';
 
 @Component({
   selector: 'app-main',
