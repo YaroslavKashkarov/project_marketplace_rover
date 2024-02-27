def87737b9022dccf442d46126b1222f9d72cef9
Leon Blyschyk <blyschyk.l@gmail.com>
2024-02-27 19:24:41 +0200

    Loyalty Program component section
    Project TeamChalenge Marketplace - ROVER. Website section - Loyalty Program.
    Includes responsive layout

---
+++ src/app/components/loyaltyprog/graph-svg/arrow-forward/arrow-forward.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-graph-arrow-forward',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './arrow-forward.component.html',
+  styleUrl: './arrow-forward.component.scss'
+})
+export class GraphArrowForwardComponent {
+
+}
---
+++ src/app/components/loyaltyprog/graph-svg/minus/minus.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-gpaph-minus',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './minus.component.html',
+  styleUrl: './minus.component.scss'
+})
+export class GraphMinusComponent {
+
+}
