1b1e6549da7613dcc1649681c3a83b2e9a23e843
CrazyMechanic <egortimchenko94@gmail.com>
2024-03-09 18:01:29 +0200

    create profile components

---
+++ src/app/components/profile/password-editing/password-editing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-password-editing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './password-editing.component.html',
+  styleUrl: './password-editing.component.scss'
+})
+export class PasswordEditingComponent {
+
+}
---
+++ src/app/components/profile/profile-editing/profile-editing.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-profile-editing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './profile-editing.component.html',
+  styleUrl: './profile-editing.component.scss'
+})
+export class ProfileEditingComponent {
+
+}
