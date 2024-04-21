7807e549e6f1e1e4d1848c2b3b75902c61c52569
CrazyMechanic <egortimchenko94@gmail.com>
2024-04-21 22:00:51 +0300

    add google btn, get user, but have error access token

--- src/app/components/social-login/social-login.component.ts
+++ src/app/components/social-login/social-login.component.ts
@@ -1,22 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { GoogleSigninButtonModule, SocialAuthService } from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider, GoogleSigninButtonModule, SocialAuthService } from '@abacritt/angularx-social-login';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
+  imports: [CommonModule, GoogleSigninButtonModule, HttpClientModule],
   selector: 'app-social-login',
   standalone: true,
-  imports: [CommonModule, GoogleSigninButtonModule],
-  templateUrl: './social-login.component.html',
   styleUrl: './social-login.component.scss',
+  templateUrl: './social-login.component.html',
 })
 export class SocialLoginComponent implements OnInit {
 
-  constructor(private authService: SocialAuthService) {
+  private accessToken = '';
+
+  constructor(private authService: SocialAuthService, private httpClient: HttpClient) {
   }
 
   ngOnInit(): void {
     this.authService.authState.subscribe((user) => {
       console.log(user);
+      this.getAccessToken();
+      this.getGoogleCalendarData();
     });
+  }
+
+  getAccessToken(): void {
+    this.authService.getAccessToken(GoogleLoginProvider.PROVIDER_ID).then(accessToken => {
+      this.accessToken = accessToken;
+      console.log(accessToken, 'asasas');
+    });
+  }
+
+  getGoogleCalendarData(): void {
+    if (!this.accessToken) return;
+
+    this.httpClient
+      .get('https://www.googleapis.com/calendar/v3/calendars/primary/events', {
+        headers: {Authorization: `Bearer ${this.accessToken}`},
+      })
+      .subscribe((events) => {
+        alert('Look at your console');
+        console.log('events', events);
+      });
   }
 }
