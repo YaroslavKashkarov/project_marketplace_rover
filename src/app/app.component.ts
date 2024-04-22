bad163e2e33a5aafdd4c80df644e449ca1be32e7
kyshin <kyshin1993@gmail.com>
2024-04-22 21:24:49 +0200

    1. Переніс CarouselComponent з AppComponent до HomePageComponent - щоб його було видно тільки на домашній сторінці 2. Переніс массив даних для відображення CarouselComponent з AppComponent до HomePageComponent.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,8 @@ import {HomePageComponent} from './components/home-page/home-page.component';
 import {FooterComponent} from './components/footer/footer.component';
 import {NavbarComponent} from './components/navbar/navbar.component';
 import {HeaderComponent} from './components/header/header.component';
-// import {
-//   CarouselComponent,
-// } from './components/home-page/carousel/carousel.component';
 import {FilterComponent} from './components/header/filter/filter.component';
-import {
-  CarouselComponent,
-} from './components/home-page/carousel/carousel.component';
+import {CarouselComponent,} from './components/home-page/carousel/carousel.component';
 
 
 @Component({
@@ -30,31 +25,5 @@ import {
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  images = [
-    {
-      imageSrc:
-        'assets/img/carousel/slide.png',
-      imageAlt: 'slide',
-      description: 'Your Perfect Bicycle Awaits!' + 'Find a bicycle for every' +
-        'path. Quality and style in one place',
-      titleButton: 'Explore Bicycle',
-    },
-    {
-      imageSrc:
-        'assets/img/carousel/slide_1.png',
-      imageAlt: 'slide1',
-      description: 'Style and Comfort on Every Ride!' + 'Enhance your look' +
-        ' and + elevate your rides with our selection of accessories and' +
-        'apparel',
-      titleButton: 'Browse Accessories',
-    },
-    {
-      imageSrc:
-        'assets/img/carousel/slide_2.png',
-      imageAlt: 'slide2',
-      title: 'Keep Your Bicycle in Perfect Condition!' + 'Parts, tools,' +
-        'and accessories for effective maintenance and repair',
-      titleButton: 'Shop Parts',
-    },
-  ];
+
 }
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -10,5 +10,31 @@ import {CarouselComponent} from './carousel/carousel.component'
   styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
-
+  images = [
+    {
+      imageSrc:
+        'assets/img/carousel/slide.png',
+      imageAlt: 'slide',
+      description: 'Your Perfect Bicycle Awaits!' + 'Find a bicycle for every' +
+        'path. Quality and style in one place',
+      titleButton: 'Explore Bicycle',
+    },
+    {
+      imageSrc:
+        'assets/img/carousel/slide_1.png',
+      imageAlt: 'slide1',
+      description: 'Style and Comfort on Every Ride!' + 'Enhance your look' +
+        ' and + elevate your rides with our selection of accessories and' +
+        'apparel',
+      titleButton: 'Browse Accessories',
+    },
+    {
+      imageSrc:
+        'assets/img/carousel/slide_2.png',
+      imageAlt: 'slide2',
+      title: 'Keep Your Bicycle in Perfect Condition!' + 'Parts, tools,' +
+        'and accessories for effective maintenance and repair',
+      titleButton: 'Shop Parts',
+    },
+  ];
 }
