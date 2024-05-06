74eb6e22e606695d990b2823b691f0ae3eed7114
Angelina <lina.shkolnaya318@gmail.com>
2024-05-06 16:04:22 +0300

    implemented saving token logic

--- src/app/components/services/authentication.service.ts
+++ src/app/components/services/authentication.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@angular/core';
 import { BaseService } from '../../../core/base.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { ISignInRequest } from '../../../core/interfaces/signin-request.interface';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { IAuthResult } from '../../../core/interfaces/auth-result.interface';
 import { ILoginRequest } from '../../../core/interfaces/login-request.interface';
 
@@ -12,19 +12,47 @@ import { ILoginRequest } from '../../../core/interfaces/login-request.interface'
 })
 export class AuthenticationService extends BaseService{
 
+  private readonly localStorageKey = 'userToken';
+
+  private userTokenSubject = new BehaviorSubject<string | null>(null);
+
   constructor(
     private socialAuthService: SocialAuthService,
     httpClient: HttpClient
   ) {
     super(httpClient);
+
+    const userDataJson = localStorage.getItem('userToken');
+
+    if (userDataJson != null) {
+      this.userTokenSubject.next(JSON.parse(userDataJson))
+    } else {
+      this.userTokenSubject.next(null);
+    }
+  }
+
+  public get userToken(): string | null {
+    return this.userTokenSubject.value;
   }
 
   registerUser(registrationRequest: ISignInRequest): Observable<IAuthResult> {
-    return this.post<IAuthResult>( 'api/auth/signup', registrationRequest);
+    return this.post<IAuthResult>( 'api/auth/signup', registrationRequest)
+    .pipe(
+      tap((res) => {
+        localStorage.setItem(this.localStorageKey, JSON.stringify(res.token));
+        this.userTokenSubject.next(res.token);
+      })
+    );
   }
 
   manualLoginUser(loginRequest: ILoginRequest): Observable<IAuthResult>{
-    return this.post<IAuthResult>('api/auth/login', loginRequest);
+    return this.post<IAuthResult>('api/auth/login', loginRequest)
+    .pipe(
+      tap((res) => {
+        localStorage.setItem(this.localStorageKey, JSON.stringify(res.token));
+        this.userTokenSubject.next(res.token);
+      })
+    );
   }
 
   loginWithGoogle(){
@@ -33,7 +61,14 @@ export class AuthenticationService extends BaseService{
         credential: res.idToken,
       }
 
-      this.sendCredential(body).subscribe({
+      this.post<IAuthResult>('api/auth/google', body)
+      .pipe(
+        tap((res) => {
+          localStorage.setItem(this.localStorageKey, JSON.stringify(res.token));
+          this.userTokenSubject.next(res.token);
+        })
+      )
+      .subscribe({
         next: (response) => {
           console.log('Authentication successful:', response);
         },
@@ -45,7 +80,4 @@ export class AuthenticationService extends BaseService{
     });
   }
 
-  sendCredential(request: any): Observable<IAuthResult>{
-    return this.post<IAuthResult>('api/auth/google', request);
-  }
 }
