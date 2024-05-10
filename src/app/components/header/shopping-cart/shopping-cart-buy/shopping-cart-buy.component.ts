14f5a339edd66873981e3f8f6ddf02382c7c0900
Yaroslav Kashkarov <126462120+YaroslavKashkarov@users.noreply.github.com>
2024-05-10 20:01:12 +0200

    fixing code style and refactoring componets

---
+++ src/app/components/header/shopping-cart/shopping-cart-buy/shopping-cart-buy.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-shopping-cart-buy',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './shopping-cart-buy.component.html',
+  styleUrl: './shopping-cart-buy.component.scss'
+})
+export class ShoppingCartBuyComponent {
+
+}
--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,14 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {CarouselComponent} from './carousel/carousel.component';
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
 	selector: 'app-home-page',
 	providers: [CarouselComponent],
 	standalone: true,
-	imports: [CommonModule, CarouselComponent],
 	templateUrl: './home-page.component.html',
 	styleUrl: './home-page.component.scss',
+	imports: [CommonModule, CarouselComponent, NavbarComponent]
 })
 
 
@@ -16,32 +17,32 @@ export class HomePageComponent implements OnInit {
 	images: any = [
 		{
 			imageSrc:
-					'assets/img/carousel/slide.png',
+				'assets/img/carousel/slide.png',
 			imageAlt: 'slide',
 			description: 'Your Perfect Bicycle Awaits!' + 'Find a bicycle for every' +
-					'path. Quality and style in one place',
+				'path. Quality and style in one place',
 			titleButton: 'Explore Bicycle',
 		},
 		{
 			imageSrc:
-					'assets/img/carousel/slide_1.png',
+				'assets/img/carousel/slide_1.png',
 			imageAlt: 'slide1',
 			description: 'Style and Comfort on Every Ride!' + 'Enhance your look' +
-					' and + elevate your rides with our selection of accessories and' +
-					'apparel',
+				' and + elevate your rides with our selection of accessories and' +
+				'apparel',
 			titleButton: 'Browse Accessories',
 		},
 		{
 			imageSrc:
-					'assets/img/carousel/slide_2.png',
+				'assets/img/carousel/slide_2.png',
 			imageAlt: 'slide2',
 			title: 'Keep Your Bicycle in Perfect Condition!' + 'Parts, tools,' +
-					'and accessories for effective maintenance and repair',
+				'and accessories for effective maintenance and repair',
 			titleButton: 'Shop Parts',
 		},
 	];
 
-	constructor(public carousel: CarouselComponent) {
+	constructor (public carousel: CarouselComponent) {
 	}
 
 	ngOnInit(): void {
