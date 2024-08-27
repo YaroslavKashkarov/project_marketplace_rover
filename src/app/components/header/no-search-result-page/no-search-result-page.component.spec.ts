ae5a2bdd165f6978be723f79a88465e67627caa9
Yevheniia Dehtiarova <Yevheniia.Dehtiarova@redsailtechnologies.com>
2024-08-27 16:36:45 +0300

    create no-search-result-comp and add behaviour

---
+++ src/app/components/header/no-search-result-page/no-search-result-page.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NoSearchResultPageComponent } from './no-search-result-page.component';
+
+describe('NoSearchResultPageComponent', () => {
+  let component: NoSearchResultPageComponent;
+  let fixture: ComponentFixture<NoSearchResultPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [NoSearchResultPageComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(NoSearchResultPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
