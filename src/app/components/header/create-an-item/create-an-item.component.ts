7072fd75936c34efb1809f258991708f72eba918
Yaroslav Kashkarov <yaroslavkashkarov@gmail.com>
2024-09-02 21:17:06 +0200

    created logic for block with loading photos

--- src/app/components/header/create-an-item/create-an-item.component.ts
+++ src/app/components/header/create-an-item/create-an-item.component.ts
@@ -10,10 +10,35 @@ import {FormsModule} from '@angular/forms';
 	styleUrl: './create-an-item.component.scss',
 })
 export class CreateAnItemComponent {
-	isCreateItemVisible: boolean = false;
+	photos: string[] = [];
 	inputText: string = '';
 	maxWords: number = 1000;
 	wordCount: number = 1;
+	index: any;
+
+	onFileInputClick(): void {
+		const fileInput = document.getElementById('photo-input') as HTMLInputElement;
+		fileInput.click();
+	}
+
+	onFileSelected(event: Event): void {
+		const input = event.target as HTMLInputElement;
+		if (input.files) {
+			Array.from(input.files).forEach(file => {
+				const reader = new FileReader();
+				reader.onload = () => {
+					if (typeof reader.result === 'string') {
+						this.photos.push(reader.result);
+					}
+				};
+				reader.readAsDataURL(file);
+			});
+		}
+	}
+
+	deleteFoto(index: number): void {
+		this.photos.splice(index, 1);
+	}
 
 	countWords(): void {
 		this.wordCount = this.inputText.length;
