a237b3457cfc2051c01e5613bbe90ed4e1ce13f4
Yevheniia Dehtiarova <Yevheniia.Dehtiarova@redsailtechnologies.com>
2024-09-21 11:51:29 +0300

    refactor methods

--- src/app/components/header/create-an-item/create-an-item.component.ts
+++ src/app/components/header/create-an-item/create-an-item.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {DollarPrefixDirective} from './directive/dollar-prefix.directive';
@@ -11,42 +11,58 @@ import {DollarPrefixDirective} from './directive/dollar-prefix.directive';
 	styleUrl: './create-an-item.component.scss',
 })
 export class CreateAnItemComponent {
-	isPhotoContented = false;
+	@ViewChild('addInput', { static: false }) addInput: ElementRef;
+	@ViewChild('photoInput', { static: false }) photoInput: ElementRef;
+
 	photos: (string | null)[] = [null, null, null];
+	setIndex: number | null;
 	inputText: string = '';
 	maxWords: number = 1000;
 	wordCount: number = 1;
 
-	onFileInputClick(): void {
-		const fileInput = document.getElementById('photo-input') as HTMLInputElement;
-		console.log(fileInput, 'input file click')
-		fileInput.click();
+	onFileInputClick(index: number | null): void {
+		if(index === null){
+			this.addInput.nativeElement.click();	
+		} else {
+			this.setIndex = index;
+			this.photoInput.nativeElement.click();
+		}
 	}
 
-	onFileSelected(event: Event, index?:number): void {
-		console.log(event, index,'file selected woks')
+	onFileSelected(event: Event): void {
 		const input = event.target as HTMLInputElement;
 		if (input.files) {
-			Array.from(input.files).forEach(file => {
-				const reader = new FileReader();
-				reader.onload = () => {
-					if (typeof reader.result === 'string') {
-						const emptyIndex = this.photos.indexOf(null);
-						if (emptyIndex !== -1) {
-						  this.photos[emptyIndex] = reader.result; 
-						}
-						this.isPhotoContented = this.photos.every(item => item !== null);
-						console.log(this.isPhotoContented)
-					}
-				};
-				reader.readAsDataURL(file);
-			});
+			Array.from(input.files).forEach(file =>this.readFile(file));
 		}
 		input.value = ''; 
 	}
 
-	deleteFoto(index: number): void {
-		this.photos.splice(index, 1);
+	private readFile(file: File): void {
+		const reader = new FileReader();
+		reader.onload = () => {
+		  if (typeof reader.result === 'string') {
+			if (this.setIndex !== null && this.setIndex !== undefined) {
+			  this.photos[this.setIndex] = reader.result; 
+			  this.setIndex = null;
+			} else {
+			  this.photos.unshift(reader.result);
+			}
+		  }
+		};
+		reader.readAsDataURL(file);
+	  }
+
+	deletePhoto(index: number | null): void {
+		console.log(index)
+		if(index !== null && index !== undefined){
+			this.photos[index] = null;
+		} else {
+			if (this.photos.length > 3 && this.photos[0] === null) {
+				this.photos.splice(0, 1);  
+			  } else {
+				this.photos.shift();  
+			  }
+		}
 	}
 
 	countWords(): void {
